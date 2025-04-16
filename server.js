55d65fe7bdcc400f3a11c2ade05de314fc14b712
FarleyHIN <rehanansa7860786@gmail.com>
2025-04-16 23:48:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+res.sendFile(path.join(__dirname, "public", "index.html"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
